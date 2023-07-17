e9f88c45452c71c67e158466735a09352c2b7c21
fgazzelloni <docksbox@pm.me>
2023-07-17 18:35:26 +0200

    Second commit - adding all files for the first week.

---
+++ scripts/00_helper.R
@@ -0,0 +1,15 @@
+# This script file is for containing basics functions and packages
+
+
+library(tidyverse)
+# GitHub.com
+library(tidytuesdayR)
+tidytuesdayR::use_tidytemplate(name = "01_introduction_bp.Rmd",
+                               refdate = "2023-05-16"
+                               )
+tidytuesdayR::use_tidytemplate(name = "02_datavisualization_bp.Rmd",
+                               refdate = "2023-05-16"
+                               )
+tidytuesdayR::use_tidytemplate(name = "03_tidytuesday_bp.Rmd")
+tidytuesdayR::use_tidytemplate(name = "04_improving_dataviz_bp.Rmd")
+tidytuesdayR::use_tidytemplate(name = "05_time_series_bp.Rmd")
